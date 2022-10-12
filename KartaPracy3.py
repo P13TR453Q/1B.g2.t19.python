dcfc4943ba2c920e7803b4e85b706709151a8c61
1b.31.PW <114579298+P13TR453Q@users.noreply.github.com>
2022-10-12 11:20:05 +0000

    pre i karta pracy 3

---
+++ KartaPracy3.py
@@ -0,0 +1,20 @@
+#pre
+#pętla liczb od 10 do 22\
+# for i in range(10, 22):
+#     print(i, end=" ")
+
+#pętla licvzb dwucyfrowych malejąco (split ujemny)
+
+# for i in range(99, 9, -1):  print(i, end=" ")
+
+#to same (split dodatni)
+
+# for i in range(-99,-9,1):  print(i*(-1),end=" ")
+
+#pętla liczb trzycyfrowych podzielnych przez 20
+
+# for i in range(100, 1000, 20):  print(i, end=" ")
+
+#Zad 1
+# a = int(input())
+# for i in range(a):  print(i, ** 3 + 3, end = " ")
