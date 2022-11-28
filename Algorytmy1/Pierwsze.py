5a30e69697c9cbb9f80cf377823f235c6d0a61ad
1b.31.PW <114579298+P13TR453Q@users.noreply.github.com>
2022-11-28 12:05:56 +0000

    Generator liczb pierwszych

--- Algorytmy1/Pierwsze.py
+++ Algorytmy1/Pierwsze.py
@@ -6,9 +6,31 @@
 #     exit()  
     
 #  print("Liczba jest pierwsza")
-a = int(input("od jakiej liczby"))
-b = int(input("do jakiej liczby"))
-for i in range(a, b):
-  for n in range(i):
-    if
-    print(i)
+ 
+# a = int(input("od ilu liczb"))
+# b = int(input("do ilu liczb"))
+# for j in range(a, b + 1):
+#   f = True
+#   for i in range(2, j):
+#     if j % i == 0:
+#       f = True
+#     else:
+#       f = False
+#       break
+#   if f == True:
+#       print(i)
+
+# n = int(input("ile liczb"))
+# k = 2
+# ilosc = 0
+# while 1:
+#   f = True
+#   for i in range(2, k):
+#     if k % i == 0:
+#       f = False
+#   if f == True:
+#       print(k)
+#     ilosc = ilosc + 1
+#   if ilosc == n:
+#     break
+#   k = k + 1
