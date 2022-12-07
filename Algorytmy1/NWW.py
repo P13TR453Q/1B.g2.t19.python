ca440fadeb3a9684d9ff5f56facda246ebe35d4d
1b.31.PW <114579298+P13TR453Q@users.noreply.github.com>
2022-12-07 12:20:35 +0000

    dodawanie ulamkow

--- Algorytmy1/NWW.py
+++ Algorytmy1/NWW.py
@@ -7,9 +7,23 @@
 #   if b > a : b = b - a
 # print(i // a)
 #Modulo
-a, b = int(input()), int(input())
-i = a * b
-while b > 0:
-  a, b = b, a % b
-print(i // a)
-    
+# a, b = int(input()), int(input())
+# i = a * b
+# while b > 0:
+#   a, b = b, a % b
+# print(i // a)
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# d = int(input())
+# x, y = a, b
+# i = x * y
+# while  x != y :
+#   if x > y : x = x - y and a * y
+#   if y > x : y = y - x and b * x
+# nww = i // x
+# e = ((nww // b) * a)
+# f = ((nww // d) * c)
+# g = (e + f)
+# print(f"{a}/{b} + {c}/{d} = {e}/{nww} + {f}/{nww} = {g}/{nww}")
+
